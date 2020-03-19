82eb30926fa1b66883c9f9e04c3906bdcc7c3a02
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-03-19 20:37:42 -0300

    ✨ Add loading component

--- client/src/Components/Loading/index.stories.js
+++ client/src/Components/Loading/index.stories.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import Loading from 'Components/Loading'
+import styled from 'styled-components'
+
+const Box = styled.div`
+  height: 500px;
+  border: 2px solid #EE6057;
+`
 
 storiesOf("Loading component", module)
-  .add("default", () => <Loading />)
+  .add("default", () => <Box><Loading /></Box>)
