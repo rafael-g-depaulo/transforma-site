ebf6897ebd23314fea1bc79efda7e6c2464a67f6
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-04-03 13:06:30 -0300

    ✨ Add Text/Title components

---
+++ client/src/Components/Text.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MyText = styled.h2`
+  font-family: 'Open Sans';
+  font-size: 18px;
+  font-weight: normal;
+  text-align: justify;
+  color: #000000;
+  column-count: ${props => props.columns ?? 1};
+  column-gap: 30px;
+`
+
+export const Text = ({
+  ...props
+}) => {
+  return (
+    <MyText {...props}/>
+  )
+}
+
+export default Text
