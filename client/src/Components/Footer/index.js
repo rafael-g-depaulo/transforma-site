c9a586bea0cf6d424ac182c6e6f970c1b087e29a
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-04-07 15:38:54 -0300

    ✨ Add empty Footer

---
+++ client/src/Components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-rows: auto auto;
+`
+
+export const Footer = ({
+  ...props
+}) => {
+  return (
+    <Grid>
+      
+    </Grid>
+  )
+}
+
+export default Footer
