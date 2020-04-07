90046f190880f815f23774bca104abd0a5351d3e
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-04-07 15:37:28 -0300

    ♻️ Refactor Loading and Divider

--- client/src/Components/Title.js
+++ client/src/Components/Title.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const MyTitle = styled.h2`
   font-family: 'Open Sans';
-  font-size: 1.6rem;
+  font-size: 2.4rem;
   font-weight: bold;
   text-align: center;
   color: #452778;
